06bf82b5ca716dc65e3b4561be88f861937d9b5b
Ryota Mine <ryotamine@gmail.com>
2019-05-17 18:03:15 +0000

    Set home link at navbar

--- my-website/src/App.js
+++ my-website/src/App.js
@@ -6,11 +6,9 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
-          <h2>Welcome to React</h2>
+          <a className="navbar-brand" href="/">RYOTA MINE</a>
+          <p>Hello</p>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
