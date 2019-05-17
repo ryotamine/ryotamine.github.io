8c05fe34a1d85a0b44d26119e4d6f935364fd7fa
Ryota Mine <ryotamine@gmail.com>
2019-05-17 20:10:52 +0000

    Add social media links to navbar

--- my-website/src/App.js
+++ my-website/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import "./App.css";
 
 class App extends Component {
+  // Render navbar
   render() {
     return (
       <div className="App">
@@ -13,6 +14,41 @@ class App extends Component {
             <a className="nav-link" href="/">SKILLS</a>
             <a className="nav-link" href="/">PROJECTS</a>
             <a className="nav-link" href="/">EMPLOYMENT</a>
+            <a className="nav-link" 
+               href="https://www.linkedin.com/in/ryotamine" 
+               target="_blank"
+            >
+              <i className="fab fa-linkedin fa-2x" 
+                 alt="Ryota Mine LinkedIn"
+              >
+              </i>
+            </a>
+            <a className="nav-link" 
+               href="https://github.com/ryotamine" 
+               target="_blank"
+            >
+              <i className="fab fa-github-square fa-2x" 
+                 alt="Ryota Mine GitHub"
+              >
+              </i>
+            </a>
+            <a className="nav-link" 
+               href="https://twitter.com/ryotamine" 
+               target="_blank"
+            >
+              <i className="fab fa-twitter-square fa-2x" 
+                 alt="Ryota Mine Twitter"
+              >
+              </i>
+            </a>
+            <a className="nav-link" 
+               href="mailto:ryotamine@gmail.com"
+            >
+              <i class="fas fa-envelope-square fa-2x" 
+                 alt="Ryota Mine Gmail"
+              >
+              </i>
+            </a>
           </nav>
         </div>
       </div>
