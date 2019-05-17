8c2f13da0f872d7372326bf2ea8924a6789fecc8
Ryota Mine <ryotamine@gmail.com>
2019-05-17 20:46:33 +0000

    Style navbar

--- my-website/src/App.js
+++ my-website/src/App.js
@@ -7,48 +7,58 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
-          <nav className="navbar">
+          <nav className="navbar fixed-top">
             <a className="navbar-brand" href="/">RYOTA MINE</a>
             <a className="nav-link" href="/">SUMMARY</a>
             <a className="nav-link" href="/">EDUCATION</a>
             <a className="nav-link" href="/">SKILLS</a>
             <a className="nav-link" href="/">PROJECTS</a>
             <a className="nav-link" href="/">EMPLOYMENT</a>
-            <a className="nav-link" 
-               href="https://www.linkedin.com/in/ryotamine" 
-               target="_blank"
-            >
-              <i className="fab fa-linkedin fa-2x" 
-                 alt="Ryota Mine LinkedIn"
-              >
-              </i>
-            </a>
-            <a className="nav-link" 
-               href="https://github.com/ryotamine" 
-               target="_blank"
-            >
-              <i className="fab fa-github-square fa-2x" 
-                 alt="Ryota Mine GitHub"
-              >
-              </i>
-            </a>
-            <a className="nav-link" 
-               href="https://twitter.com/ryotamine" 
-               target="_blank"
-            >
-              <i className="fab fa-twitter-square fa-2x" 
-                 alt="Ryota Mine Twitter"
-              >
-              </i>
-            </a>
-            <a className="nav-link" 
-               href="mailto:ryotamine@gmail.com"
-            >
-              <i class="fas fa-envelope-square fa-2x" 
-                 alt="Ryota Mine Gmail"
-              >
-              </i>
-            </a>
+            <form className="form-inline">
+              <div className="linkedin">
+                <a className="nav-link" 
+                  href="https://www.linkedin.com/in/ryotamine" 
+                  target="_blank"
+                >
+                  <i className="fab fa-linkedin fa-2x" 
+                    alt="Ryota Mine LinkedIn"
+                  >
+                  </i>
+                </a>
+              </div>
+              <div className="github">
+                <a className="nav-link" 
+                  href="https://github.com/ryotamine" 
+                  target="_blank"
+                >
+                  <i className="fab fa-github-square fa-2x" 
+                    alt="Ryota Mine GitHub"
+                  >
+                  </i>
+                </a>
+              </div>
+              <div className="twitter">
+                <a className="nav-link" 
+                  href="https://twitter.com/ryotamine" 
+                  target="_blank"
+                >
+                  <i className="fab fa-twitter-square fa-2x" 
+                    alt="Ryota Mine Twitter"
+                  >
+                  </i>
+                </a>
+              </div>
+              <div className="gmail">
+                <a className="nav-link" 
+                  href="mailto:ryotamine@gmail.com"
+                >
+                  <i class="fas fa-envelope-square fa-2x" 
+                    alt="Ryota Mine Gmail"
+                  >
+                  </i>
+                </a>
+              </div>
+            </form>
           </nav>
         </div>
       </div>
