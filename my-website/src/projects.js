66b46c87fc10aadfea50376262cdb6b7c5a2ba46
Ryota Mine <ryotamine@gmail.com>
2019-05-21 19:25:26 +0000

    Add hyperlinks and responsive design to projects

--- my-website/src/projects.js
+++ my-website/src/projects.js
@@ -1,5 +1,9 @@
 import React, { Component } from "react";
 import surfparrot from "../docs/surfparrot.png";
+import jungle from "../docs/ruby-on-rails.png";
+import chattyApp from "../docs/react.png";
+import schoodle from "../docs/schoodle.png";
+import tinyApp from "../docs/node.png";
 
 class Projects extends Component {
   // Render projects page
@@ -35,6 +39,130 @@ class Projects extends Component {
                   </li>
                   <li>
                     Considered UI/UX adequately to meet intuitive use
+                  </li>
+                </ul>
+              </p>
+              <br></br>
+            </div>
+          </div>
+        </a>
+        <a className="jungle"
+           href="https://github.com/ryotamine/jungle-rails"
+           target="_blank"
+        >
+          <div className="form-row">
+            <div className="form-group col-md-4">
+              <img
+                src={jungle}
+                className="rounded mx-auto d-block"
+                alt="Jungle"
+              >
+              </img>
+            </div>
+            <div className="form-group col-md-8">
+              <h3>Jungle</h3>
+              <p>
+                Apr 2019
+                <br></br>
+                <ul>
+                  <li>
+                    Updated full-stack e-commerce application using Ruby on Rails in 24 hours
+                  </li>
+                  <li>
+                    Navigated through MVC accordingly to meet user needs
+                  </li>
+                </ul>
+              </p>
+              <br></br>
+            </div>
+          </div>
+        </a>
+        <a className="chatty-app"
+           href="https://github.com/ryotamine/chatty-app"
+           target="_blank"
+        >
+          <div className="form-row">
+            <div className="form-group col-md-4">
+              <img
+                src={chattyApp}
+                className="rounded mx-auto d-block"
+                alt="Chatty App"
+              >
+              </img>
+            </div>
+            <div className="form-group col-md-8">
+              <h3>Chatty App</h3>
+              <p>
+                Apr 2019
+                <br></br>
+                <ul>
+                  <li>
+                    Implemented full-stack online chat using React in 24 hours
+                  </li>
+                  <li>
+                    Applied WebPack and Express deliberately for bi-directional communication
+                  </li>
+                </ul>
+              </p>
+              <br></br>
+            </div>
+          </div>
+        </a>
+        <a className="schoodle"
+           href="https://github.com/ryotamine/schoodle"
+           target="_blank"
+        >
+          <div className="form-row">
+            <div className="form-group col-md-4">
+              <img
+                src={schoodle}
+                className="rounded mx-auto d-block"
+                alt="Schoodle"
+              >
+              </img>
+            </div>
+            <div className="form-group col-md-8">
+              <h3>Schoodle</h3>
+              <p>
+                Apr 2019
+                <br></br>
+                <ul>
+                  <li>
+                    Produced full-stack event scheduler using HTML, CSS, JavaScript, jQuery, AJAX, Node, Express, SQL, and Knex in 48 hours
+                  </li>
+                  <li>
+                    Managed front-end, back-end, and database tasks consistently from start to finish
+                  </li>
+                </ul>
+              </p>
+              <br></br>
+            </div>
+          </div>
+        </a>
+        <a className="tiny-app"
+           href="https://github.com/ryotamine/tinyapp"
+           target="_blank"
+        >
+          <div className="form-row">
+            <div className="form-group col-md-4">
+              <img
+                src={tinyApp}
+                className="rounded mx-auto d-block"
+                alt="TinyApp"
+              >
+              </img>
+            </div>
+            <div className="form-group col-md-8">
+              <h3>TinyApp</h3>
+              <p>
+                Mar 2019
+                <br></br>
+                <ul>
+                  <li>
+                    Created full-stack URL shortener using JavaScript, Node, and Express in 24 hours
+                  </li>
+                  <li>
+                    Debugged code sufficiently to meet altering needs
                   </li>
                 </ul>
               </p>
