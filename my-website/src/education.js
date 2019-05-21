16fb8e409e68c3d405847888e717861782fa0259
Ryota Mine <ryotamine@gmail.com>
2019-05-21 21:22:10 +0000

    Change hyperlinks to pictures only for education, projects, and employment

--- my-website/src/education.js
+++ my-website/src/education.js
@@ -12,62 +12,62 @@ class Education extends Component {
           <h3>Lighthouse Labs | University of Waterloo</h3>
           <hr></hr>
         </div>
-        <a className="lighthouse-labs"
-           href="https://lighthouselabs.ca/"
-           target="_blank"
-        >
-          <div className="form-row">
-            <div className="form-group col-md-4">
+        <div className="form-row">
+          <div className="form-group col-md-4">
+            <a className="lighthouse-labs"
+                href="https://lighthouselabs.ca/"
+                target="_blank"
+            >
               <img
                 src={lighthouseLabs}
                 className="rounded mx-auto d-block"
                 alt="Lighthouse Labs"
               >
               </img>
-            </div>
-            <div className="form-group col-md-8">
-              <h3>Lighthouse Labs</h3>
-              <p>
-                <strong>
-                  Diploma, Web Development
-                </strong>
-                <br></br>
-                Mar - May 2019
-                <br></br>
-                Toronto, Ontario, Canada
-              </p>
-              <br></br>
-            </div>
+            </a>
           </div>
-        </a>
-        <a className="university-of-waterloo"
-           href="https://uwaterloo.ca/"
-           target="_blank"
-        >
-          <div className="form-row">
-            <div className="form-group col-md-4">
+          <div className="form-group col-md-8">
+            <h3>Lighthouse Labs</h3>
+            <p>
+              <strong>
+                Diploma, Web Development
+              </strong>
+              <br></br>
+              Mar - May 2019
+              <br></br>
+              Toronto, Ontario, Canada
+            </p>
+            <br></br>
+          </div>
+        </div>
+        <div className="form-row">
+          <div className="form-group col-md-4">
+            <a className="university-of-waterloo"
+                href="https://uwaterloo.ca/"
+                target="_blank"
+            >
               <img
                 src={universityOfWaterloo}
                 className="rounded mx-auto d-block"
                 alt="University of Waterloo"
               >
               </img>
-            </div>
-            <div className="form-group col-md-8">
-              <h3>University of Waterloo</h3>
-              <p>
-                <strong>
-                  Bachelor of Applied Science, Honours Mechanical Engineering Co-op
-                </strong>
-                <br></br>
-                2013
-                <br></br>
-                Waterloo, Ontario, Canada
-              </p>
-              <br></br>
-            </div>
+            </a>
           </div>
-        </a>
+          <div className="form-group col-md-8">
+            <h3>University of Waterloo</h3>
+            <p>
+              <strong>
+                Bachelor of Applied Science, Honours Mechanical Engineering Co-op
+              </strong>
+              <br></br>
+              2013
+              <br></br>
+              Waterloo, Ontario, Canada
+            </p>
+            <br></br>
+          </div>
+        </div>
       </div>
     );
   }
--- my-website/src/projects.js
+++ my-website/src/projects.js
@@ -15,161 +15,161 @@ class Projects extends Component {
           <h3>Full-Stack Web Developer</h3>
           <hr></hr>
         </div>
-        <a className="surfparrot"
-           href="https://github.com/ryotamine/surfparrot"
-           target="_blank"
-        >
-          <div className="form-row">
-            <div className="form-group col-md-4">
+        <div className="form-row">
+          <div className="form-group col-md-4">
+            <a className="surfparrot"
+                href="https://github.com/ryotamine/surfparrot"
+                target="_blank"
+            >
               <img
                 src={surfparrot}
                 className="rounded mx-auto d-block"
                 alt="Surfparrot"
               >
               </img>
-            </div>
-            <div className="form-group col-md-8">
-              <h3>Surfparrot</h3>
-              <p>
-                May 2019
-                <br></br>
-                <ul>
-                  <li>
-                    Generated full-stack live-music event listing preview using React, Node, Express, PostgreSQL, Axios, and Spotify API in 96 hours
-                  </li>
-                  <li>
-                    Considered UI/UX adequately to meet intuitive use
-                  </li>
-                </ul>
-              </p>
-              <br></br>
-            </div>
+            </a>
           </div>
-        </a>
-        <a className="jungle"
-           href="https://github.com/ryotamine/jungle-rails"
-           target="_blank"
-        >
-          <div className="form-row">
-            <div className="form-group col-md-4">
+          <div className="form-group col-md-8">
+            <h3>Surfparrot</h3>
+            <p>
+              May 2019
+              <br></br>
+              <ul>
+                <li>
+                  Generated full-stack live-music event listing preview using React, Node, Express, PostgreSQL, Axios, and Spotify API in 96 hours
+                </li>
+                <li>
+                  Considered UI/UX adequately to meet intuitive use
+                </li>
+              </ul>
+            </p>
+            <br></br>
+          </div>
+        </div>
+        <div className="form-row">
+          <div className="form-group col-md-4">
+            <a className="jungle"
+                href="https://github.com/ryotamine/jungle-rails"
+                target="_blank"
+            >
               <img
                 src={jungle}
                 className="rounded mx-auto d-block"
                 alt="Jungle"
               >
               </img>
-            </div>
-            <div className="form-group col-md-8">
-              <h3>Jungle</h3>
-              <p>
-                Apr 2019
-                <br></br>
-                <ul>
-                  <li>
-                    Updated full-stack e-commerce application using Ruby on Rails in 24 hours
-                  </li>
-                  <li>
-                    Navigated through MVC accordingly to meet user needs
-                  </li>
-                </ul>
-              </p>
-              <br></br>
-            </div>
+            </a>
           </div>
-        </a>
-        <a className="chatty-app"
-           href="https://github.com/ryotamine/chatty-app"
-           target="_blank"
-        >
-          <div className="form-row">
-            <div className="form-group col-md-4">
+          <div className="form-group col-md-8">
+            <h3>Jungle</h3>
+            <p>
+              Apr 2019
+              <br></br>
+              <ul>
+                <li>
+                  Updated full-stack e-commerce application using Ruby on Rails in 24 hours
+                </li>
+                <li>
+                  Navigated through MVC accordingly to meet user needs
+                </li>
+              </ul>
+            </p>
+            <br></br>
+          </div>
+        </div>
+        <div className="form-row">
+          <div className="form-group col-md-4">
+            <a className="chatty-app"
+                href="https://github.com/ryotamine/chatty-app"
+                target="_blank"
+            >
               <img
                 src={chattyApp}
                 className="rounded mx-auto d-block"
                 alt="Chatty App"
               >
               </img>
-            </div>
-            <div className="form-group col-md-8">
-              <h3>Chatty App</h3>
-              <p>
-                Apr 2019
-                <br></br>
-                <ul>
-                  <li>
-                    Implemented full-stack online chat using React in 24 hours
-                  </li>
-                  <li>
-                    Applied WebPack and Express deliberately for bi-directional communication
-                  </li>
-                </ul>
-              </p>
-              <br></br>
-            </div>
+            </a>
           </div>
-        </a>
-        <a className="schoodle"
-           href="https://github.com/ryotamine/schoodle"
-           target="_blank"
-        >
-          <div className="form-row">
-            <div className="form-group col-md-4">
+          <div className="form-group col-md-8">
+            <h3>Chatty App</h3>
+            <p>
+              Apr 2019
+              <br></br>
+              <ul>
+                <li>
+                  Implemented full-stack online chat using React in 24 hours
+                </li>
+                <li>
+                  Applied WebPack and Express deliberately for bi-directional communication
+                </li>
+              </ul>
+            </p>
+            <br></br>
+          </div>
+        </div>
+        <div className="form-row">
+          <div className="form-group col-md-4">
+            <a className="schoodle"
+                href="https://github.com/ryotamine/schoodle"
+                target="_blank"
+            >
               <img
                 src={schoodle}
                 className="rounded mx-auto d-block"
                 alt="Schoodle"
               >
               </img>
-            </div>
-            <div className="form-group col-md-8">
-              <h3>Schoodle</h3>
-              <p>
-                Apr 2019
-                <br></br>
-                <ul>
-                  <li>
-                    Produced full-stack event scheduler using HTML, CSS, JavaScript, jQuery, AJAX, Node, Express, SQL, and Knex in 48 hours
-                  </li>
-                  <li>
-                    Managed front-end, back-end, and database tasks consistently from start to finish
-                  </li>
-                </ul>
-              </p>
-              <br></br>
-            </div>
+            </a>
           </div>
-        </a>
-        <a className="tiny-app"
-           href="https://github.com/ryotamine/tinyapp"
-           target="_blank"
-        >
-          <div className="form-row">
-            <div className="form-group col-md-4">
+          <div className="form-group col-md-8">
+            <h3>Schoodle</h3>
+            <p>
+              Apr 2019
+              <br></br>
+              <ul>
+                <li>
+                  Produced full-stack event scheduler using HTML, CSS, JavaScript, jQuery, AJAX, Node, Express, SQL, and Knex in 48 hours
+                </li>
+                <li>
+                  Managed front-end, back-end, and database tasks consistently from start to finish
+                </li>
+              </ul>
+            </p>
+            <br></br>
+          </div>
+        </div>
+        <div className="form-row">
+          <div className="form-group col-md-4">
+            <a className="tiny-app"
+                href="https://github.com/ryotamine/tinyapp"
+                target="_blank"
+            >
               <img
                 src={tinyApp}
                 className="rounded mx-auto d-block"
                 alt="TinyApp"
               >
               </img>
-            </div>
-            <div className="form-group col-md-8">
-              <h3>TinyApp</h3>
-              <p>
-                Mar 2019
-                <br></br>
-                <ul>
-                  <li>
-                    Created full-stack URL shortener using JavaScript, Node, and Express in 24 hours
-                  </li>
-                  <li>
-                    Debugged code sufficiently to meet altering needs
-                  </li>
-                </ul>
-              </p>
-              <br></br>
-            </div>
+            </a>
           </div>
-        </a>
+          <div className="form-group col-md-8">
+            <h3>TinyApp</h3>
+            <p>
+              Mar 2019
+              <br></br>
+              <ul>
+                <li>
+                  Created full-stack URL shortener using JavaScript, Node, and Express in 24 hours
+                </li>
+                <li>
+                  Debugged code sufficiently to meet altering needs
+                </li>
+              </ul>
+            </p>
+            <br></br>
+          </div>
+        </div>
       </div>
     );
   }
