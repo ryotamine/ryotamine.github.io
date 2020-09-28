9b7972c17c5b5adee42f0a1f89691d382c26c59f
Ryota Mine <ryotamine@gmail.com>
2020-09-28 15:18:02 -0400

    Update skills component of the landing page

--- my-website/src/components/Skills.js
+++ my-website/src/components/Skills.js
@@ -1,130 +1,146 @@
-import React, { Component } from "react";
+// Libraries
+import React, { useState } from "react";
+
+// Styling
 import html from "../../docs/html.png";
 import css from "../../docs/css.png";
-import javaScript from "../../docs/javascript.png";
+import javascript from "../../docs/javascript.png";
 import ruby from "../../docs/ruby.png";
-import express from "../../docs/express.png";
-import jQuery from "../../docs/jquery.png";
 import react from "../../docs/react.png";
+import bootstrap from "../../docs/bootstrap.png";
+import node from "../../docs/node.png";
+import express from "../../docs/express.png";
 import rubyOnRails from "../../docs/ruby-on-rails.png";
-import mongoDb from "../../docs/mongo-db.png";
+import postgresql from "../../docs/postgresql.png";
 import sql from "../../docs/sql.png";
 import gitAndGithub from "../../docs/git-and-github.png";
-import node from "../../docs/node.png";
 
-class Skills extends Component {
-  // Render skills page
-  render() {
-    return (
-      <div id="skills" className="responsive">
-        <div className="headline">
-          <h1>Skills</h1>
-          <h3>Front-End | Back-End | Database</h3>
-          <hr></hr>
+const Skills = () => {
+  // Set initial state
+  const [load, setLoad] = useState(false);
+
+  // Render Skills Component
+  return (
+    <div id="skills">
+      <div className="text-center">
+        <div className="section-name">Skills</div>
+        <div className="headline">Front-End | Back-End | Database</div>
+        <hr></hr>
+      </div>
+      <div className="row">
+        <div className="col-xl-2 col-md-4 col-sm-6">
+          <img
+            src={html}
+            alt="HTML"
+            onLoad={() => setLoad(true)}
+            className={load ? "skills-image" : "loading-image"}
+          />
+          <div className="skills-name">HTML</div>
         </div>
-        <div className="form-row">
-          <div className="form-group col-md-2">
-            <img 
-              src={html}
-              className="rounded mx-auto d-block"
-              alt="HTML"
-            >
-            </img>
-          </div>
-          <div className="form-group col-md-2">
-            <img 
-              src={css}
-              className="rounded mx-auto d-block"
-              alt="CSS"
-            >
-            </img>
-          </div>
-          <div className="form-group col-md-2">
-            <img 
-              src={javaScript}
-              className="rounded mx-auto d-block"
-              alt="JavaScript"
-            >
-            </img>
-          </div>
-          <div className="form-group col-md-2">
-            <img 
-              src={ruby}
-              className="rounded mx-auto d-block"
-              alt="Ruby"
-            >
-            </img>
-          </div>
-          <div className="form-group col-md-2">
-            <img 
-              src={express}
-              className="rounded mx-auto d-block"
-              alt="Express"
-            >
-            </img>
-          </div>
-          <div className="form-group col-md-2">
-            <img 
-              src={jQuery}
-              className="rounded mx-auto d-block"
-              alt="jQuery"
-            >
-            </img>
-          </div>
+        <div className="col-xl-2 col-md-4 col-sm-6">
+          <img
+            src={css}
+            alt="CSS"
+            onLoad={() => setLoad(true)}
+            className={load ? "skills-image" : "loading-image"}
+          />
+          <div className="skills-name">CSS</div>
         </div>
-        <div className="form-row">
-          <div className="form-group col-md-2">
-            <img 
-              src={react}
-              className="rounded mx-auto d-block"
-              alt="React"
-            >
-            </img>
-          </div>
-          <div className="form-group col-md-2">
-            <img 
-              src={rubyOnRails}
-              className="rounded mx-auto d-block"
-              alt="Ruby On Rails"
-            >
-            </img>
-          </div>
-          <div className="form-group col-md-2">
-            <img 
-              src={mongoDb}
-              className="rounded mx-auto d-block"
-              alt="MongoDB"
-            >
-            </img>
-          </div>
-          <div className="form-group col-md-2">
-            <img 
-              src={sql}
-              className="rounded mx-auto d-block"
-              alt="SQL"
-            >
-            </img>
-          </div>
-          <div className="form-group col-md-2">
-            <img 
-              src={gitAndGithub}
-              className="rounded mx-auto d-block"
-              alt="Git and GitHub"
-            >
-            </img>
-          </div>
-          <div className="form-group col-md-2">
-            <img 
-              src={node}
-              className="rounded mx-auto d-block"
-              alt="Node"
-            >
-            </img>
-          </div>
+        <div className="col-xl-2 col-md-4 col-sm-6">
+          <img
+            src={javascript}
+            alt="JavaScript"
+            onLoad={() => setLoad(true)}
+            className={load ? "skills-image" : "loading-image"}
+          />
+          <div className="skills-name">JavaScript</div>
+        </div>
+        <div className="col-xl-2 col-md-4 col-sm-6">
+          <img
+            src={ruby}
+            alt="Ruby"
+            onLoad={() => setLoad(true)}
+            className={load ? "skills-image" : "loading-image"}
+          />
+          <div className="skills-name">Ruby</div>
+        </div>
+        <div className="col-xl-2 col-md-4 col-sm-6">
+          <img
+            src={react}
+            alt="React"
+            onLoad={() => setLoad(true)}
+            className={load ? "skills-image" : "loading-image"}
+          />
+          <div className="skills-name">React</div>
+        </div>
+        <div className="col-xl-2 col-md-4 col-sm-6">
+          <img
+            src={bootstrap}
+            alt="Bootstrap"
+            onLoad={() => setLoad(true)}
+            className={load ? "skills-image" : "loading-image"}
+          />
+          <div className="skills-name">Bootstrap</div>
         </div>
       </div>
-    );
-  }
-}
+      <div className="row">
+        <div className="col-xl-2 col-md-4 col-sm-6">
+          <img 
+            src={node} 
+            alt="Node"
+            onLoad={() => setLoad(true)}
+            className={load ? "skills-image" : "loading-image"}
+          />
+          <div className="skills-name">Node</div>
+        </div>
+        <div className="col-xl-2 col-md-4 col-sm-6">
+          <img
+            src={express}
+            alt="Express"
+            onLoad={() => setLoad(true)}
+            className={load ? "skills-image" : "loading-image"}
+          />
+          <div className="skills-name">Express</div>
+        </div>
+        <div className="col-xl-2 col-md-4 col-sm-6">
+          <img
+            src={rubyOnRails}
+            alt="Ruby On Rails"
+            onLoad={() => setLoad(true)}
+            className={load ? "skills-image" : "loading-image"}
+          />
+          <div className="skills-name">Ruby on Rails</div>
+        </div>
+        <div className="col-xl-2 col-md-4 col-sm-6">
+          <img
+            src={postgresql}
+            alt="PostgreSQL"
+            onLoad={() => setLoad(true)}
+            className={load ? "skills-image" : "loading-image"}
+          />
+          <div className="skills-name">PostgreSQL</div>
+        </div>
+        <div className="col-xl-2 col-md-4 col-sm-6">
+          <img 
+            src={sql} 
+            alt="SQL"
+            onLoad={() => setLoad(true)}
+            className={load ? "skills-image" : "loading-image"}
+          />
+          <div className="skills-name">SQL</div>
+        </div>
+        <div className="col-xl-2 col-md-4 col-sm-6">
+          <img
+            src={gitAndGithub}
+            alt="Git and GitHub"
+            onLoad={() => setLoad(true)}
+            className={load ? "skills-image" : "loading-image"}
+          />
+          <div className="skills-name">Git and GitHub</div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Skills;
