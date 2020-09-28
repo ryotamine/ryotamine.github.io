19226abe8441c63b983022615e0dbb973503978e
Ryota Mine <ryotamine@gmail.com>
2020-09-28 14:06:58 -0400

    Update profile component of the landing page

--- my-website/src/components/Profile.js
+++ my-website/src/components/Profile.js
@@ -1,51 +1,54 @@
-import React, { Component } from "react";
+// Libraries
+import React, { useState } from "react";
+
+// Styling
 import profilePicture from "../../docs/ryota-mine.png";
 
-class Profile extends Component {
-  // Render profile page
-  render() {
-    return (
-      <div id="profile" className="responsive">
-        <div className="headline">
-          <h1>Profile</h1>
-          <h3>Full-Stack Web Developer | Mechanical Engineering</h3>
-          <hr></hr>
+const Profile = () => {
+  // Set initial state
+  const [load, setLoad] = useState(false);
+
+  // Render Profile Component
+  return (
+    <div id="profile">
+      <div className="text-center">
+        <div className="section-name">Profile</div>
+        <div className="headline">Product Manager | Mechanical Engineering</div>
+        <hr></hr>
+      </div>
+      <div className="row">
+        <div className="mb-3 col-lg-4">
+          <img
+            src={profilePicture}
+            alt="Ryota Mine"
+            onLoad={() => setLoad(true)}
+            className={load ? "profile-picture" : "loading-image"}
+          />
         </div>
-        <div className="form-row">
-          <div className="form-group col-md-4">
-            <img 
-              src={profilePicture}
-              className="rounded mx-auto d-block"
-              alt="Ryota Mine"
-            >
-            </img>
+        <div className="mb-3 col-lg-8">
+          <div className="title">Summary</div>
+          <div className="mb-3">
+            Product Manager with 5 years of Mechanical Engineering background in
+            the automotive industry (mainly on interior parts). I have been
+            fascinated with how software programs are designed, developed and
+            released to improve the workplace. This curiosity led me to pursue a
+            career in software development. I am a continuous learner with
+            profound leadership and project management skills. I also have
+            international work experience in the USA and Mexico.
           </div>
-          <div id="info" className="form-group col-md-8">
-            <h3>Summary</h3>
-            <p>
-              Junior Full-Stack Web Developer with 5 years of Mechanical Engineering background in the automotive industry (mainly on interior parts). I have been fascinated with how software programs are designed, developed, and released to improve the workplace. This curiosity led me to pursue a career in full-stack development. I am a continuous learner with profound leadership and project management skills. I also have international work experience in the USA and Mexico.
-            </p>
-            <br></br>
-            <h3>Details</h3>
-            <p>
-              <strong>Name:</strong>
-              <br></br>
-              Ryota Mine
-              <br></br>
-              <strong>Location:</strong>
-              <br></br>
-              Toronto, Ontario, Canada
-              <br></br>
-              <strong>Industry:</strong>
-              <br></br>
-              Computer Software
-            </p>
-            <br></br>
+          <div className="title">Details</div>
+          <div>
+            <strong>Name</strong>
+            <div>Ryota Mine</div>
+            <strong>Location</strong>
+            <div>Toronto, Ontario, Canada</div>
+            <strong>Industry</strong>
+            <div>Computer Software</div>
           </div>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default Profile;
