11c7ea4b39f0ce12b9aa6341eb000b4ccd03e25c
Ryota Mine <ryotamine@gmail.com>
2020-09-28 14:38:47 -0400

    Update education component of the landing page

--- my-website/src/components/Education.js
+++ my-website/src/components/Education.js
@@ -1,72 +1,70 @@
-import React, { Component } from "react";
+// Libraries
+import React, { useState } from "react";
+
+// Styling
 import lighthouseLabs from "../../docs/lighthouse-labs.png";
 import universityOfWaterloo from "../../docs/university-of-waterloo.png";
 
-class Education extends Component {
-  // Render education page
-  render() {
-    return (
-      <div id="education" className="responsive">
-        <div className="headline">
-          <h1>Education</h1>
-          <h3>Lighthouse Labs | University of Waterloo</h3>
-          <hr></hr>
+const Education = () => {
+  // Set initial state
+  const [load, setLoad] = useState(false);
+
+  // Render Education Component
+  return (
+    <div id="education">
+      <div className="text-center">
+        <div className="section-name">Education</div>
+        <div className="headline">Lighthouse Labs | University of Waterloo</div>
+        <hr></hr>
+      </div>
+      <div className="row">
+        <div className="mb-3 col-lg-4">
+          <a
+            href="https://lighthouselabs.ca/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              src={lighthouseLabs}
+              alt="Lighthouse Labs"
+              onLoad={() => setLoad(true)}
+              className={load ? "lighthouse-labs" : "loading-image"}
+            />
+          </a>
         </div>
-        <div className="form-row">
-          <div className="form-group col-md-4">
-            <a 
-              className="lighthouse-labs"
-              href="https://lighthouselabs.ca/"
-              target="_blank"
-            >
-              <img 
-                src={lighthouseLabs}
-                className="rounded mx-auto d-block"
-                alt="Lighthouse Labs"
-              >
-              </img>
-            </a>
-          </div>
-          <div className="form-group col-md-8">
-            <h3>Lighthouse Labs</h3>
-            <strong>
-              Diploma, Web Development
-            </strong>
-            <br></br>
-            Mar - May 2019
-            <br></br>
-            Toronto, Ontario, Canada
-          </div>
-        </div>
-        <div className="form-row">
-          <div className="form-group col-md-4">
-            <a 
-              className="university-of-waterloo"
-              href="https://uwaterloo.ca/"
-              target="_blank"
-            >
-              <img 
-                src={universityOfWaterloo}
-                className="rounded mx-auto d-block"
-                alt="University of Waterloo"
-              >
-              </img>
-            </a>
-          </div>
-          <div className="form-group col-md-8">
-            <h3>University of Waterloo</h3>
-            <strong>
-              Bachelor of Applied Science, Honours Mechanical Engineering Co-op
-            </strong>
-            <br></br>
-            2013
-            <br></br>
-            Waterloo, Ontario, Canada
-          </div>
+        <div className="mb-3 col-lg-8">
+          <div className="title">Lighthouse Labs</div>
+          <strong>Diploma, Web Development</strong>
+          <div>Mar - May 2019</div>
+          <div>Toronto, Ontario, Canada</div>
         </div>
       </div>
-    );
-  }
-}
+      <div className="row">
+        <div className="mb-3 col-lg-4">
+          <a
+            href="https://uwaterloo.ca/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              src={universityOfWaterloo}
+              alt="University of Waterloo"
+              onLoad={() => setLoad(true)}
+              className={load ? "university-of-waterloo" : "loading-image"}
+            />
+          </a>
+        </div>
+        <div className="mb-3 col-lg-8">
+          <div className="title">University of Waterloo</div>
+          <strong>
+            Bachelor of Applied Science, Honours Mechanical Engineering Co-op
+          </strong>
+          <div>2013</div>
+          <div>Waterloo, Ontario, Canada</div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Education;
