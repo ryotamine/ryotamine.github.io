f1567cdbed161ca03f3002cf765fd41c4d40059f
Ryota Mine <ryotamine@gmail.com>
2020-09-28 10:11:25 -0400

    Add navbar styling effect

--- my-website/src/components/Navbar.js
+++ my-website/src/components/Navbar.js
@@ -1,140 +1,176 @@
-import React, { Component } from "react";
-import { Link, animateScroll as scroll } from "react-scroll";
+// Libraries
+import React, { useState, useEffect } from "react";
+import { Link } from "react-scroll";
 
-class Navbar extends Component {
-  // Scroll to top of home page
-  scrollToTop = () => {
-    scroll.scrollToTop();
-  };
+const Navbar = () => {
+  // Set initial state
+  const [navbarEffect, setNavbarEffect] = useState(false);
 
-  // Render home page
-  render() {
-    return (
-      <div className="App-header">
-        <nav className="navbar fixed-top">
-          <form className="form-inline">
-            <a 
-              className="navbar-brand"
-              onClick={this.scrollToTop}
+  // Mount Navbar
+  useEffect(() => {
+    window.scrollTo(0, 0);
+
+    // Mount Navbar styling effect
+    window.addEventListener("scroll", () => {
+      const isTop = window.scrollY < window.innerHeight - 70;
+
+      if (isTop) {
+        setNavbarEffect(false);
+      } else {
+        setNavbarEffect(true);
+      }
+    });
+
+    // Unmount Navbar styling effect at scroll
+    window.removeEventListener("scroll", () => {});
+  }, []);
+
+  // Render Navbar
+  return (
+    <div id="landing-page">
+      <nav className={navbarEffect ? "navbar-effect" : "navbar"}>
+        <div className="navbar-logo">
+          <Link
+            activeClass="active"
+            to="landing-page"
+            spy={true}
+            smooth={true}
+            offset={0}
+            duration={0}
+          > 
+            RYOTA MINE
+          </Link>
+        </div>
+
+        <div className="navbar-spacing" />
+
+        {/* <div className="linkedin">
+          <a 
+            className="nav-link" 
+            href="https://www.linkedin.com/in/ryotamine" 
+            target="_blank"
+          >
+            <i 
+              className="fab fa-linkedin fa-2x" 
+              alt="Ryota Mine LinkedIn"
             >
-              RYOTA MINE
-            </a>
-            <div className="linkedin">
-              <a 
-                className="nav-link" 
-                href="https://www.linkedin.com/in/ryotamine" 
-                target="_blank"
-              >
-                <i 
-                  className="fab fa-linkedin fa-2x" 
-                  alt="Ryota Mine LinkedIn"
-                >
-                </i>
-              </a>
-            </div>
-            <div className="github">
-              <a 
-                className="nav-link" 
-                href="https://github.com/ryotamine" 
-                target="_blank"
-              >
-                <i 
-                  className="fab fa-github-square fa-2x" 
-                  alt="Ryota Mine GitHub"
-                >
-                </i>
-              </a>
-            </div>
-            <div className="twitter">
-              <a 
-                className="nav-link" 
-                href="https://twitter.com/ryotamine" 
-                target="_blank"
-              >
-                <i 
-                  className="fab fa-twitter-square fa-2x" 
-                  alt="Ryota Mine Twitter"
-                >
-                </i>
-              </a>
-            </div>
-            <div className="gmail">
-              <a 
-                className="nav-link" 
-                href="mailto:ryotamine@gmail.com"
-              >
-                <i 
-                  className="fas fa-envelope-square fa-2x" 
-                  alt="Ryota Mine Gmail"
-                >
-                </i>
-              </a>
-            </div>
-          </form>
-          <div className="section">
-            <Link
-              activeClass="active"
-              to="profile"
-              spy={true}
-              smooth={true}
-              offset={-50}
-              duration={500}
-            > 
-              PROFILE
-            </Link>
-          </div>
-          <div className="section">
-            <Link
-              activeClass="active"
-              to="education"
-              spy={true}
-              smooth={true}
-              offset={-50}
-              duration={500}
-            > 
-              EDUCATION
-            </Link>
-          </div>
-          <div className="section">
-            <Link
-              activeClass="active"
-              to="skills"
-              spy={true}
-              smooth={true}
-              offset={-50}
-              duration={500}
-            > 
-              SKILLS
-            </Link>
-          </div>
-          <div className="section">
-            <Link
-              activeClass="active"
-              to="projects"
-              spy={true}
-              smooth={true}
-              offset={-50}
-              duration={500}
-            > 
-              PROJECTS
-            </Link>
-          </div>
-          <div className="section">
-            <Link
-              activeClass="active"
-              to="employment"
-              spy={true}
-              smooth={true}
-              offset={-50}
-              duration={500}
-            > 
-              EMPLOYMENT
-            </Link>
-          </div>
-        </nav>
-      </div>
-    );
-  }
+            </i>
+          </a>
+        </div>
+        <div className="github">
+          <a 
+            className="nav-link" 
+            href="https://github.com/ryotamine" 
+            target="_blank"
+          >
+            <i 
+              className="fab fa-github-square fa-2x" 
+              alt="Ryota Mine GitHub"
+            >
+            </i>
+          </a>
+        </div>
+        <div className="twitter">
+          <a 
+            className="nav-link" 
+            href="https://twitter.com/ryotamine" 
+            target="_blank"
+          >
+            <i 
+              className="fab fa-twitter-square fa-2x" 
+              alt="Ryota Mine Twitter"
+            >
+            </i>
+          </a>
+        </div>
+        <div className="gmail">
+          <a 
+            className="nav-link" 
+            href="mailto:ryotamine@gmail.com"
+          >
+            <i 
+              className="fas fa-envelope-square fa-2x" 
+              alt="Ryota Mine Gmail"
+            >
+            </i>
+          </a>
+        </div> */}
+
+        <div className="navbar-section">
+          <Link
+            activeClass="active"
+            to="profile"
+            spy={true}
+            smooth={true}
+            offset={-70}
+            duration={0}
+          > 
+            PROFILE
+          </Link>
+        </div>
+        <div className="navbar-section">
+          <Link
+            activeClass="active"
+            to="education"
+            spy={true}
+            smooth={true}
+            offset={-70}
+            duration={0}
+          > 
+            EDUCATION
+          </Link>
+        </div>
+        <div className="navbar-section">
+          <Link
+            activeClass="active"
+            to="skills"
+            spy={true}
+            smooth={true}
+            offset={-70}
+            duration={0}
+          > 
+            SKILLS
+          </Link>
+        </div>
+        <div className="navbar-section">
+          <Link
+            activeClass="active"
+            to="employment"
+            spy={true}
+            smooth={true}
+            offset={-70}
+            duration={0}
+          > 
+            EMPLOYMENT
+          </Link>
+        </div>
+        <div className="navbar-section">
+          <Link
+            activeClass="active"
+            to="projects"
+            spy={true}
+            smooth={true}
+            offset={-70}
+            duration={0}
+          > 
+            PROJECTS
+          </Link>
+        </div>
+        <div className="navbar-section">
+          <Link
+            activeClass="active"
+            to="contact"
+            spy={true}
+            smooth={true}
+            offset={-70}
+            duration={0}
+          > 
+            CONTACT
+          </Link>
+        </div>
+      </nav>
+    </div>
+  );
 }
 
 export default Navbar;
